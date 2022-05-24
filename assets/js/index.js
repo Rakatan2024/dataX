3b6d86dcb6293f4b7e6d7e0cbbe36739e0040fd3
Rakhat789 <roshakbaiev@mail.ru>
2022-05-24 10:49:59 +0600

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+
+
+
+$("#add_user").submit(function(){
+    alert("Data Inserted Successfully!");
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3000/admin/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(){
+        alert("Data Updated Successfully!");
+    })
+
+})
+
+if(window.location.pathname === "/admin"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/admin/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        }
+
+    })
+}
